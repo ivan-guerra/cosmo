02a164df9297af461c652a3f708fba59cc90974e
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2022-01-23 19:53:42 -0800

    Cleanup kernel main such that it just demos logging to the framebuffer.

--- kernel/kmain.cc
+++ kernel/kmain.cc
@@ -1,48 +1,21 @@
-#include "SerialPort.h"
+#include "Logger.h"
 #include "FrameBuffer.h"
-
-void FillStr(char* str, unsigned int len, char fill)
-{
-    if (!str)
-        return;
-
-    for (unsigned int i = 0; i < len; ++i)
-        str[i] = fill;
-}
-
-unsigned int StrLen(const char* str) {
-    if (!str)
-        return 0;
-
-    unsigned int len = 0;
-    for (int i = 0; str[i]; ++i)
-        len++;
-
-    return len;
-}
 
 int main(void)
 {
     cosmo::FrameBuffer fb;
     fb.ClearScreen();
 
-    const char* kWelcomeText = "Welcome to cosmo OS!";
-    fb.PrintString(kWelcomeText, StrLen(kWelcomeText));
+    /* Sample values to be logged. */
+    char         cvalue = '#';
+    int          ivalue = -42;
+    unsigned int uvalue = 0xffffffff;
+    const char*  svalue = "cosmo OS";
 
-    const char* kSerialWriteNotice = "\nWriting serial data...";
-    fb.PrintString(kSerialWriteNotice, StrLen(kSerialWriteNotice));
-
-    const char* kSerialMsg = "Hello, world.\nThis is cosmo OS speaking!";
-    const char* kSerialErrorMsg = "\nUnable to initialize serial port!";
-    const char* kSerialSuccessMsg = "\nData sent to serial port successfully!";
-
-    cosmo::SerialPort sp;
-    if (!sp.Init()) {
-        fb.PrintString(kSerialErrorMsg, StrLen(kSerialErrorMsg));
-    } else {
-        fb.PrintString(kSerialSuccessMsg, StrLen(kSerialSuccessMsg));
-        sp.PrintString(kSerialMsg, StrLen(kSerialMsg));
-    }
+    cosmo::Logger logger;
+    logger.LogInfo(fb,
+        "cvalue = %c\nivalue = %d\nuvalue = %u\nuvalue(hex) = %X\nsvalue = %s\n",
+        cvalue, ivalue, uvalue, uvalue, svalue);
 
     return 0;
 }
