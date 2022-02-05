bd445294c08e96fd787ac40c412fbb7725fb7758
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2022-02-04 16:46:12 -0800

    Register a generic IRQ handler with all 16 hardware interrupt slots in the IDT.
    Just how we registered a generic ISR handler with the IDT, we repeat the
    process for IRQs. The irq_handler() method will switch on the interrupt
    number and proceed to execute the appropriate C++ function for that IRQ.
    Note, the 16 IRQ entries immediately follow the 32 ISR entries in the
    IDT.

--- src/InterruptDescriptorTable/InterruptHandler.cc
+++ src/InterruptDescriptorTable/InterruptHandler.cc
@@ -1,4 +1,6 @@
 #include "InterruptHandler.h"
+#include "PortIO.h"
+#include "ProgrammableInterruptController.h"
 #include "Logger.h"
 #include "FrameBuffer.h"
 
@@ -7,10 +9,26 @@ namespace cosmo
 void interrupt::isr_handler(struct InterruptContext* int_context)
 {
     cosmo::FrameBuffer fb;
-    fb.ClearScreen();
-
     cosmo::Logger logger;
     logger.LogError(fb, "Exception %X occurred.",
                     static_cast<unsigned int>(int_context->int_no));
+}
+
+void interrupt::irq_handler(struct InterruptContext* int_context)
+{
+    cosmo::FrameBuffer fb;
+    cosmo::Logger logger;
+    /* TODO: Implement a complete switch statement. */
+    static int i = 0;
+    switch(int_context->int_no) {
+        case Irq::kKeyboard:
+            i++;
+            logger.LogError(fb, "KBD Interrupt %d occurred.", i);
+            cosmo::Pic::SendEOI(1);
+            break;
+        default:
+            logger.LogError(fb, "IRQ %d occurred.",
+                            static_cast<int>(int_context->int_no));
+    }
 }
 } // end cosmo
